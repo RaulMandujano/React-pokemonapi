5462ac5fb3f2e011481ef32a1e35ebbf2216079b
Raul Mandujano <raulmandujano@Rauls-Mac-mini.local>
2023-02-08 13:51:52 -0700

    pokemon api

---
+++ src/Components/Main.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import Card from "./Card";
+import Pokeinfo from "./Pokeinfo";
+import axios from "axios";
+import { useState } from "react";
+// import { useEffect } from "react";
+
+const Main=()=>{
+    const [pokeData,setPokeData] = useState([]);
+    const [loading,setLoading] = useState(true)
+    const [url,setUrl] = useState ("https://pokeapi.co/api/v2/pokemon/")
+
+    const pokeFun = async()=>{
+        setLoading(true)
+        const res=await axios.get(url);
+        console.log(res.data)
+    }
+
+    useEffect(()=>{
+        pokeFun();
+    },[url])
+
+    return(
+        <>
+            <div className="container">
+                <div className="left-content">
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <div className="btn-group">
+                        <button>Previous</button>
+                        <button>Next</button>
+                    </div>
+                </div>
+                <div className="right-content">
+                    <Pokeinfo/>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Main;
